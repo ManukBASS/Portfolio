e8dd080a327bc500b66aefd4333ae9efdbeac8df
Manu Zarraga <manuzarraga@hotmail.com>
2023-10-28 15:53:12 -0300

    edit: changed Pit-Shop description

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 /*===== REMOVE MENU MOBILE =====*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show')
 }
@@ -25,17 +25,17 @@ const sections = document.querySelectorAll('section[id]')
 
 window.addEventListener('scroll', scrollActive)
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
         }
     })
@@ -51,30 +51,31 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title', {})
-sr.reveal('.home__scroll', {delay: 200})
-sr.reveal('.home__img', {origin:'right', delay: 400})
+sr.reveal('.home__scroll', { delay: 200 })
+sr.reveal('.home__img', { origin: 'right', delay: 400 })
+sr.reveal('.section-title', { origin: 'top', distance: '80px', duration: 1400 });
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
-sr.reveal('.about__subtitle', {delay: 300})
-sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
-sr.reveal('.about__social-icon', {delay: 600, interval: 100})
-sr.reveal('.about__cv', {delay: 900, interval: 300})
+sr.reveal('.about__img', { delay: 500 })
+sr.reveal('.about__subtitle', { delay: 300 })
+sr.reveal('.about__profession', { delay: 400 })
+sr.reveal('.about__text', { delay: 500 })
+sr.reveal('.about__social-icon', { delay: 600, interval: 100 })
+sr.reveal('.about__cv', { delay: 900, interval: 300 })
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__tech', {distance: '20px', interval: 100})
-sr.reveal('.skills__img', {delay: 400})
+sr.reveal('.skills__tech', { distance: '20px', interval: 100 })
+sr.reveal('.skills__img', { delay: 400 })
 
 /*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', {interval: 100})
+sr.reveal('.portfolio__img', { interval: 100 })
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', {interval: 200})
-sr.reveal('.contact__input', {delay: 400})
-sr.reveal('.contact__button', {delay: 600})
+sr.reveal('.contact__text', { interval: 200 })
+sr.reveal('.contact__input', { delay: 400 })
+sr.reveal('.contact__button', { delay: 600 })
 
 /*MANDY*/
 
