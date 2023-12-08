5fe875da0afe5f7b8bf3f0360fdaa9f722cc0007
Manu Zarraga <manuzarraga@hotmail.com>
2023-12-08 02:34:53 -0300

    feat: adding react navite project, implemented project mapping from json

--- js/main.js
+++ js/main.js
@@ -69,7 +69,8 @@ sr.reveal('.skills__tech', { distance: '20px', interval: 100 })
 sr.reveal('.skills__img', { delay: 400 })
 
 /*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', { interval: 100 })
+sr.reveal('.computer-icon', { interval: 200 })
+sr.reveal('.mobile-icon', { interval: 200 })
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
---
+++ js/projectMap.js
@@ -0,0 +1,71 @@
+const computerProjects = [];
+const mobileProjects = [];
+
+function renderComputerProjects() {
+    renderProjects(computerProjects, 'portfolio-container');
+}
+
+function renderMobileProjects() {
+    renderProjects(mobileProjects, 'portfolio-container');
+}
+
+function renderProjects(projects, containerId, isMobile) {
+    const portfolioContainer = document.getElementById(containerId);
+    portfolioContainer.innerHTML = '';
+
+    projects.forEach(project => {
+        const isDevelopment = project.title === 'En desarrollo...';
+
+        const githubIcon = isDevelopment ? '<i class="bx bxs-error bx-lg"></i>' : `<a target="_blank" href="${project.githubLink}"><i class='bx bxl-github bx-lg'></i></a>`;
+        const toolsIcons = project.tools.length > 0 && !isDevelopment ? project.tools.map(tool => `<i class='bx bxl-${tool.toLowerCase()} bx-lg'></i>`).join('') : '';
+        const mobileIcon = project['other-tools'] && project['other-tools'].includes('BX-MOBILE-ALT') && !isDevelopment ? `<i class='bx bx-mobile-alt bx-lg'></i>` : '';
+
+        const projectCard = `
+            <div class="portfolio__img ${isMobile ? 'mobile-project' : ''}">
+                <h2>${project.title}</h2>
+                <img src="${project.image}" alt="${project.title}" class="${isMobile ? 'mobile-image' : ''}">
+                <div class="portfolio__info">
+                    <p>${project.description}</p>
+                    <div class="portfolio__links">
+                        ${project.liveLink ? `<a target="_blank" href="${project.liveLink}"><i class='bx bx-link bx-lg'></i></a>` : ''}
+                        ${githubIcon}
+                    </div>
+                </div>
+                <div class="portfolio-tools">
+                    ${toolsIcons}
+                    ${mobileIcon}
+                </div>
+            </div>
+        `;
+        portfolioContainer.innerHTML += projectCard;
+    });
+
+    sr.reveal('.portfolio__img', { interval: 100 });
+}
+
+
+function filterProjects(category) {
+    if (category === 'computer') {
+        renderComputerProjects();
+    } else if (category === 'mobile') {
+        renderMobileProjects();
+    }
+}
+
+axios.get('/json/projects.json')
+    .then(response => {
+        computerProjects.push(...response.data);
+        filterProjects('computer');
+    })
+    .catch(error => console.error('Error fetching computer projects:', error));
+
+axios.get('/json/projectsMobile.json')
+    .then(response => {
+        mobileProjects.push(...response.data);
+        filterProjects('mobile');
+    })
+    .catch(error => console.error('Error fetching mobile projects:', error))
+    .finally(() => {
+        renderComputerProjects();
+    });
+
